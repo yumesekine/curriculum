4da5d12ef9ccccffb9a3918f13bf4ed773551533
yumesekine <yuramepop@yahoo.co.jp>
2020-08-01 02:17:17 +0900

    配列
    Task1_2/java

--- JavaStudy/1-2/Task1_2.java
+++ JavaStudy/1-2/Task1_2.java
@@ -25,8 +25,11 @@ public class Task1_2 {
          *   2番目の要素に 「もも」 を代入
          *   3番目の要素に 「ぶどう」 を代入
          */
-         String[] strArray = {"りんご","もも","ぶどう"};
-
+         String[] strArray = new String[3] 
+         strArray[0] = "りんご";
+         strArray[1] = "もも";
+         strArray[2] = "ぶどう";
+         
         // ④ ③で作成した配列の2番目の要素を出力しなさい。
         System.out.println(strArray[1]);
 
